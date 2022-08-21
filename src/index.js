a7e686360682a96850530915ed669eb632252dad
shannone <shannone426@gmail.com>
2022-08-21 13:01:36 -0400

    added fetch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,51 @@
 fetch('http://localhost:3000/pups')
 .then(response => response.json())
-.then(pups => {
-
-    
+.then((data) => {
+  
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // })
+
+// .then((data) => {
+//   console.log(data)
+//   renderHeader(data)
+//   renderFooter(data)
+// function pupCard(data) {
+//   const pup = document.querySelector('#dog-bar')
+
+//   const pupList = document.createElement('span')
+
+//   pupList.textContent = pups.name
+
+//   pup.append(pupList)
+
+// };
+
+
+// function pupImg(puppy) {
+// const img = document.createElement("img")
+// img.src = pups.image
+// img.addEventListener('click', () => {
+//   showMovieDetails(puppy)
+// })
+//  pupImg.append(img)
+// }
